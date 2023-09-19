522c5c47e994b9eb207e25754c13a57c28ce184d
Mélyssa Auberton <melyssa.auberton@gmail.com>
2023-09-19 11:05:44 +0200

    update on news scraping

--- index.js
+++ index.js
@@ -1,27 +1,53 @@
-const express= require('express');
+// const express= require('express');
 const cheerio= require('cheerio');
 const axios= require('axios');
 
-const PORT= 8000;
+// const PORT= 8000;
 const url= "https://www.francetvinfo.fr/";
 
-const app= express();
+// const app= express();
+
+const article= [];
 
 axios(url)
     .then(response => {
         const html= response.data;
         const $= cheerio.load(html);
-        const articles= [];
-        $('.most-read__item', html).each(function(){
-            const title= $(this).find('p').text();
+        
+        $('.card-article-majeure', html).each(function(){
+            const title= $(this).find('h2').text();
             const href= $(this).find('a').attr('href');
-            articles.push({
+            article.push({
                 title,
                 href
             });
-        })
-       console.log(articles);
+        });
+       return article;
     })
+    .then(data => {
+        axios("https://www.francetvinfo.fr" + data[0].href)
+        .then(response=> {
+            const html= response.data;
+            const $= cheerio.load(html);
+            
+            $('article', html).each(function(){
+                const subtitle= $(this).find('.c-chapo').text();
+                const img= $(this).find('img').attr('src');
+                const content= $(this).find('.c-body').find('p').text();
+                //récupérer aussi date et auteur...
+                article.push({"data" : {
+                    subtitle,
+                    img,
+                    content
+                }});
+            })
+            console.log(article);
+        })
+        .catch(err=> console.log("err : " + err));
+    })
+
     .catch(err => console.log(err));
 
-app.listen(PORT, ()=> console.log(`server running on PORT ${PORT}`));
+    // console.log(article);
+
+// app.listen(PORT, ()=> console.log(`server running on PORT ${PORT}`));
